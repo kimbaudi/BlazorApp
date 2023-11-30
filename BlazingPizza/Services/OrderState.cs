61a30ab725ce120349f58bb348dfee6f9ce322a8
Paul Kim <kimbaudi@gmail.com>
2023-11-30 10:37:46 -0800

    add top nav. allow customers to place orders. add checkout page.

--- BlazingPizza/Services/OrderState.cs
+++ BlazingPizza/Services/OrderState.cs
@@ -1,4 +1,4 @@
-﻿namespace BlazingPizza.Services;
+namespace BlazingPizza.Services;
 
 public class OrderState
 {
@@ -37,5 +37,10 @@ public class OrderState
     public void RemoveConfiguredPizza(Pizza pizza)
     {
         Order.Pizzas.Remove(pizza);
+    }
+
+    public void ResetOrder()
+    {
+        Order = new Order();
     }
 }
